d23c067c6230e8c2b4f6d80dd491ac8e943a6105
abukimemia <akimesh51@gmail.com>
2022-04-06 14:29:13 +0300

    Add Auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import { getPosts } from './actions/posts';
-import useStyles from './styles';
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Auth from "./components/Auth/Auth";
 
-const App = () => {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-  const classes = useStyles();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
-  return (
+const App = () => (
+  <BrowserRouter>
     <Container maxWidth="lg">
       <Navbar />
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
     </Container>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
